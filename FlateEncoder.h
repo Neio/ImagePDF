7bfce7fa69236f783c648e9dd9a250855179aa9a
Neio <neio.zhou@gmail.com>
2012-11-21 14:22:05 +0800

    modify license

--- FlateEncoder.h
+++ FlateEncoder.h
@@ -2,9 +2,21 @@
 //  FlateEncoder.h
 //  ImagePDF
 //
-//  Created by 周聪辉 on 11/20/12.
-//  Copyright (c) 2012 Neio. All rights reserved.
-//
+/*
+ Copyright 2012 Neio Zhou
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 
 #ifndef __ImagePDF__FlateEncoder__
 #define __ImagePDF__FlateEncoder__
